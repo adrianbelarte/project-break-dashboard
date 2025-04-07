9a00215102ee17a9760187a28e0fda4d6ba88665
Adrian Belarte <adrian.belarte.it@gmail.com>
2025-04-08 01:48:16 +0200

    empezamos relojo

--- js/links.js
+++ js/links.js
@@ -1,1 +1,75 @@
-console.log("funciona")
+const tituloInput = document.getElementById(`tituloInput`);
+const linkInput = document.getElementById(`linkInput`);
+const confirmar = document.querySelector(`.btn-confirmar`)
+const linkList = document.querySelector(`.linkList`)
+let links = [];
+
+iniciarPagina();
+
+//boton para confirmar el enlace
+confirmar.addEventListener(`click`, () => {
+    let titulo = tituloInput.value;
+    let link = linkInput.value;
+    if (!titulo || !link) return;
+
+    guardarLink(titulo,link);
+    clearInputs();
+    templateEnlace(links)
+    guardarLocalStorage();
+})
+
+//creacion del enlace
+function templateEnlace(links){
+    linkList.innerHTML = ""; 
+
+    links.forEach((linkData, index) =>{
+        let linkCard = document.createElement(`div`);
+        linkCard.classList.add(`link-card`);
+
+        linkCard.innerHTML = `
+        <span class="close-btn" title="Eliminar">
+        <i class="fas fa-times"></i>
+        </span>
+        <a href="${linkData.link}" target="_blank">
+        <i class="fas fa-link"></i> ${linkData.titulo}
+        </a>
+        `;
+        
+        linkList.appendChild(linkCard);
+        linkCard.querySelector(".close-btn").addEventListener("click", () => {
+            eliminarLink(index);
+        });
+    })
+}
+
+// meter el titulo y en link
+function guardarLink(titulo, link) {
+    links.push({ titulo, link });
+}
+
+//borrar los inputs
+function clearInputs(){
+    tituloInput.value = "";
+    linkInput.value = "";
+}
+
+//cargar los links a iniciar la pagina
+function iniciarPagina(){
+    const storedLinks = localStorage.getItem(`linksGuardados`);
+    if(storedLinks){
+        links = JSON.parse(storedLinks);
+        templateEnlace(links);
+    }
+}
+
+//guardar en el locaStorage
+function guardarLocalStorage(){
+    localStorage.setItem(`linksGuardados`, JSON.stringify(links));
+}
+
+// Eliminar link y actualizar
+function eliminarLink(index) {
+    links.splice(index, 1);
+    guardarLocalStorage();
+    templateEnlace(links);
+}
--- js/password.js
+++ js/password.js
@@ -4,6 +4,8 @@ const mayusculas =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const minusculas =  "abcdefghijklmnopqrstuvwxyz";
 const numeros = "0123456789";
 const simbolos = "!@#$%^&*()-_=+";
+const longitudPassword = document.getElementById("nCaracteres")
+const confirmar = document.querySelector(`.confirmar`)
 
 // Math.random() Para generar aleatoriedad
 function getRandom(arr){
@@ -12,9 +14,9 @@ function getRandom(arr){
 }
 
 // guardar minimo los 4 datos diferentes
-function password(n){
+function generarPassword(n){
     if(n >= 12 && n <= 50){
-        let password = [
+        let chars = [
             getRandom(mayusculas),
             getRandom(minusculas),
             getRandom(numeros),
@@ -23,12 +25,12 @@ function password(n){
 
         const todos = mayusculas + minusculas + numeros + simbolos;
         for (let i = 4; i < n; i++) {
-            password.push(getRandom(todos));
+            chars.push(getRandom(todos));
         }
 
-        return shuffle(password.join(``));
+        return shuffle(chars.join(``));
     }
-    return false;
+    return "longitud de password no valida"
 }
 
 //mezclar los datos
@@ -38,6 +40,35 @@ function shuffle(str){
     .sort(() => Math.random() - 0.5)
     .join(``)
 }
+
+//muestra los datos en el html
+function templatePassword(txt){
+    const password = document.getElementById(`password`)
+    password.innerHTML = `${txt}`
+}
+
+
+
+
+// numeros de 12 a 50 en el selector
+function nCaracteres(selectElement, min = 12, max = 50) {
+    for (let i = min; i <= max; i++) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectElement.appendChild(option);
+    }
+}
+nCaracteres(longitudPassword)
+
+
+// botton para confirmar la contraseña seguroa
+confirmar.addEventListener(`click`, () => {
+    const cantidad = parseInt(longitudPassword.value);
+    const newPassword = generarPassword(cantidad);
+    templatePassword(newPassword);
+})
+
 
 
 
